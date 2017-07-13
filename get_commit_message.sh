501c155a0a9301bb44c3753d8b0d33dcf56026ec
Joe Loughry <joe.loughry@gmail.com>
2017-07-13 10:28:01 -0600

    limit line length on commit messages

--- get_commit_message.sh
+++ get_commit_message.sh
@@ -5,7 +5,10 @@ message="commit_message.txt"
 rm -f $message
 touch $message
 
+/bin/echo "12345678901234567890123456789012345678901234567890"
+/bin/echo "         1         2         3         4         5"
 /bin/echo "Ready to commit; give me a message (it can be multiple lines); end with '.'"
+/bin/echo "[At most fifty characters is recommended.]"
 /bin/echo -n "> "
 
 read line
